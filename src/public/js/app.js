e997e8dd344d1a44123ae2b22e5d695344be83bc
comstudynews <comstudynews@gmail.com>
2022-06-10 10:49:17 +0900

    chapter06-04

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
 const socket = io();
 
 const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
 async function getMedia() {
     try {
@@ -10,9 +14,40 @@ async function getMedia() {
             audio: true, video:true
         });
         console.log(myStream);
+        myFace.srcObject = myStream;
     } catch(e) {
         console.log(e);
     }
 }
 
 getMedia();
+
+function mediaToggle(target) {
+    //console.log(myStream.getAudioTracks());
+    myStream[target]().forEach( media => (media.enabled = !media.enabled) );
+    //myStream[target]()[0].enabled = !(myStream[target]()[0].enabled);
+}
+
+muteBtn.addEventListener('click', function() {
+    mediaToggle("getAudioTracks");
+    if(!muted) {
+        muteBtn.innerText = "Unmute";
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+});
+
+cameraBtn.addEventListener('click', function() {
+    //console.log(myStream.getVideoTracks());
+    //myStream.getVideoTracks().forEach( media => (media.enabled = !media.enabled) );
+    mediaToggle("getVideoTracks");
+    if(!cameraOff) {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    } else {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    }
+});
