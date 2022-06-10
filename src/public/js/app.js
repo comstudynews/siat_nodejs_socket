052fd8e01e799bf4bce7e9f015e77a9e8fa45ddf
comstudynews <comstudynews@gmail.com>
2022-06-10 14:31:41 +0900

    chapter07-5

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -117,17 +117,19 @@ const welcomeForm = welcome.querySelector('form');
 
 call.hidden = true;
 
-async function startMedia() {
+//async function startMedia() {
+async function initCall() {
     welcome.hidden = true;
     call.hidden = false;
     await getMedia();
     makeConnection();
 }
 
-welcomeForm.addEventListener('submit', function(event) {
+welcomeForm.addEventListener('submit', async function(event) {
     event.preventDefault();
     const input = welcomeForm.querySelector("input");
-    socket.emit("join_room", input.value, startMedia);
+    await initCall();
+    socket.emit("join_room", input.value);
     roomName = input.value;
     input.value = "";
 });
@@ -142,14 +144,47 @@ socket.on("welcome", async ()=>{
     socket.emit("offer", offer, roomName);
 });
 
-socket.on("offer", offer => {
-    console.log(offer);
-})
+socket.on("offer", async (offer) => {
+    console.log("received the offer");
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    console.log(answer);
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the offer");
+});
+
+socket.on("answer", answer => {
+    console.log("received the answer");
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", ice => {
+    console.log("received candidate");
+    myPeerConnection.addIceCandidate(ice);
+});
 
 
 // RCT Code
 function makeConnection() {
     myPeerConnection = new RTCPeerConnection();
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
     console.log(myStream.getTracks());
     myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce(data) {
+    console.log("sent candidate");
+    socket.emit("ice", data.candidate, roomName);
+    // console.log("got ice candidate");
+    // console.log(data);
+}
+
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");
+    peerFace.srcObject = data.stream;
+    // console.log("got a sream from peer");
+    // console.log("Peer's Stream", data.stream);
+    // console.log("My Stream", myStream);
 }
--- src/server.js
+++ src/server.js
@@ -19,10 +19,16 @@ server.listen(app.get("port"), ()=> {
 wsServer.on("connection", (socket)=> {
     socket.on("join_room", (roomName, done)=>{
         socket.join(roomName);
-        done();
+        // done();
         socket.to(roomName).emit("welcome");
     });
     socket.on("offer", (offer, roomName) => {
         socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
     });
 });
